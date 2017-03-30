bd06c67f0434d421db86effcde920da4dc4b52a9
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-03-30 23:33:44 +0530

    network state check added

--- app/src/main/java/com/rishabhshukla/popularmoviesapp/view/MainActivity.java
+++ app/src/main/java/com/rishabhshukla/popularmoviesapp/view/MainActivity.java
@@ -1,5 +1,8 @@
 package com.rishabhshukla.popularmoviesapp.view;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -50,30 +53,37 @@ public class MainActivity extends AppCompatActivity {
         movieAdapter = new MovieAdapter(singleMovieArrayList);
 
         TheMoviesDBApi theMovieDbApi = new TheMoviesDBApi();
-        theMovieDbApi.getMovieClient().getMovieList().enqueue(new Callback<MovieList>() {
-            @Override
-            public void onResponse(Call<MovieList> call, Response<MovieList> response) {
-                for (SingleMovie singleMovie : response.body().getResults()){
-                    Log.e("Movie",singleMovie.getOriginalTitle());
-                    singleMovieArrayList.add(singleMovie);
+        if(isOnline()) {
+            theMovieDbApi.getMovieClient().getMovieList().enqueue(new Callback<MovieList>() {
+                @Override
+                public void onResponse(Call<MovieList> call, Response<MovieList> response) {
+                    for (SingleMovie singleMovie : response.body().getResults()) {
+                        Log.e("Movie", singleMovie.getOriginalTitle());
+                        singleMovieArrayList.add(singleMovie);
+                    }
+                    movieAdapter.notifyDataSetChanged();
+
                 }
-                movieAdapter.notifyDataSetChanged();
 
-            }
+                @Override
+                public void onFailure(Call<MovieList> call, Throwable t) {
 
-            @Override
-            public void onFailure(Call<MovieList> call, Throwable t) {
+                }
 
-            }
-        });
-
+            });
+        }
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movieRecyclerView);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this,2);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(movieAdapter);
 
     }
-
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
