77194b7913d0f526ebb22800f9b327e1f76e7324
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-03-30 23:52:58 +0530

    layout cardview

--- app/src/main/java/com/rishabhshukla/popularmoviesapp/rest/MovieClient.java
+++ app/src/main/java/com/rishabhshukla/popularmoviesapp/rest/MovieClient.java
@@ -11,7 +11,7 @@ import retrofit2.http.GET;
 
 public interface MovieClient {
 
-    @GET("movie/popular?api_key=97f469b9e89b30f1f7d07e8b35973e56&page=1&language=en-US")
+    @GET("movie/popular?api_key=c49a0ac24db60b4efb6b0d46f212a7f2")
     Call<MovieList> getMovieList();
 
 }
