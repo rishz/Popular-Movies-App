8050823e5d06d36b657e97384ad2106bb0c3369c
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-03-30 22:42:14 +0530

    recycler view

--- app/src/main/java/com/rishabhshukla/popularmoviesapp/view/MainActivity.java
+++ app/src/main/java/com/rishabhshukla/popularmoviesapp/view/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -64,6 +66,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movieRecyclerView);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(movieAdapter);
 
     }
 
