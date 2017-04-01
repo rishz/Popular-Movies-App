f55578f8b4639c4aae120b8772f6b89ff10b3c23
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-04-01 23:52:29 +0530

    colors

--- app/src/main/java/com/rishabhshukla/popularmoviesapp/view/MainActivity.java
+++ app/src/main/java/com/rishabhshukla/popularmoviesapp/view/MainActivity.java
@@ -6,8 +6,6 @@ import android.content.res.Configuration;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -17,7 +15,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import com.rishabhshukla.popularmoviesapp.R;
@@ -54,16 +51,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         singleMovieArrayList = new ArrayList<>();
         movieAdapter = new MovieAdapter(singleMovieArrayList);
 
