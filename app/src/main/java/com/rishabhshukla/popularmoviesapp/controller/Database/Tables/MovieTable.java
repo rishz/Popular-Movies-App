9bb8da3292273a5cf14141b576e8fcf5760fdb2f
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-04-09 19:57:36 +0530

    sqlite table

--- app/src/main/java/com/rishabhshukla/popularmoviesapp/controller/Database/Tables/MovieTable.java
+++ app/src/main/java/com/rishabhshukla/popularmoviesapp/controller/Database/Tables/MovieTable.java
@@ -7,16 +7,34 @@ import static com.rishabhshukla.popularmoviesapp.controller.Database.Consts.*;
  */
 
 public class MovieTable {
-    public static final String TABLE_NAME = "todos";
-    private static final String TAG = "TODOS";
-    public static final String CMD_UPDATE_TABLE_1_2_ADD_COL = "ALTER TABLE "+ TABLE_NAME + " ADD "+Columns.DONE+
-            TYPE_BOOLEAN+SEMICOL;
+    public static final String TABLE_NAME = "movies";
+    private static final String TAG = "MOVIE_TABLE";
+
+//    public static final String CMD_UPDATE_TABLE_1_2_ADD_COL = "ALTER TABLE "+ TABLE_NAME + " ADD "+Columns.DONE+
+//            TYPE_BOOLEAN+SEMICOL;
+
     public static final String CMD_TABLE_CREATE =   "CREATE TABLE "+TABLE_NAME+
-            LBR+Columns.ID+TYPE_INT+TYPE_PK+TYPE_AI+COMMA+Columns.TASK+TYPE_TEXT+RBR+SEMICOL;
+            LBR+Columns.ID+TYPE_INT+TYPE_PK+COMMA+Columns.posterPath+TYPE_TEXT+COMMA+
+        Columns.overview+TYPE_TEXT+COMMA+Columns.releaseDate+TYPE_TEXT +
+        COMMA+Columns.originalTitle+TYPE_TEXT+COMMA+Columns.title+TYPE_TEXT+COMMA +
+        Columns.backdropPath+TYPE_TEXT+COMMA+Columns.originalLanguage+TYPE_TEXT+
+        COMMA+Columns.adult+TYPE_BOOLEAN+COMMA+Columns.voteCount+TYPE_INT +
+        RBR+SEMICOL;
 
     public interface  Columns{
-        String ID = "todo_id";
-        String TASK  = "todo_task";
-        String DONE = "todo_done";
+        String ID = "movie_id";
+        String posterPath = "movie_posterPath";
+        String overview = "movie_overview";
+
+        String releaseDate = "movie_releaseDate";
+        String originalTitle = "movie_originalTitle";
+         String title = "movie_title";
+         String backdropPath = "movie_backdropPath";
+         String originalLanguage = "movie_originalLanguage";
+        String adult = "movie_adult";
+        String popularity = "movie_popularity";
+         String voteAvg = "movie_voteAvg";
+         String voteCount = "movie_voteCount";
+         String video = "movie_video";
     }
 }
